0694f3358758f74c2f86ca8661dd29405d6f4569
cuong <cuong16102002@gmail.com>
2023-06-10 21:24:54 +0700

    Initial Commit

---
+++ SuggestBusStation/SuggestBusStationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuggestBusStationApp.swift
+//  SuggestBusStation
+//
+//  Created by Macbook Pro on 10/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct SuggestBusStationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
